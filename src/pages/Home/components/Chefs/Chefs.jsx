fe145932ac337a825db2a1052711d9bdd7f311cc
nazmus767921 <nazmus767921@gmail.com>
2023-09-26 13:33:06 +0600

    U:A: separated Chef comp styles

--- src/pages/Home/components/Chefs/Chefs.jsx
+++ src/pages/Home/components/Chefs/Chefs.jsx
@@ -1,13 +1,13 @@
-import styled from "styled-components";
 import ChefCard from "./components/ChefCard";
-import { devices } from "../../../../utils/breakpoints";
 import { chefsDetails } from "../../../../constant/en-us/home_page.constants";
 import Title from "../../../../components/UI/Title";
 import Tag from "../../../../components/UI/Tag";
+import { Wrapper } from "./styles/Chef.styles";
 
 const Chefs = () => {
 	return (
 		<Wrapper>
+			<div className="background"></div>
 			<div className="header">
 				<Tag>{chefsDetails.tagline}</Tag>
 				<Title>{chefsDetails.title}</Title>
@@ -25,37 +25,4 @@ const Chefs = () => {
 	);
 };
 
-const Wrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	flex-wrap: wrap;
-	padding-block: 6.825em;
-	border: 1px solid;
-	/* for making the container 100vw */
-	width: 100vw;
-	margin: 0 calc(-50vw + 50%);
-	/* for making the container 100vw */
-  background-image: url();
-
-	.header {
-		display: flex;
-		flex-direction: column;
-		gap: 0.625em;
-	}
-
-	.cards_container {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-	}
-
-	.chefCard--wrapper {
-		width: 47%;
-		@media screen and (${devices.xl}) {
-			width: 30%;
-		}
-	}
-`;
 export default Chefs;
---
+++ src/pages/Home/components/Chefs/styles/Chef.styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { devices } from "../../../../../utils/breakpoints";
+import BGImage from "../assets/chicken-sticks-bowl-tomato-soup.jpg";
+
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+	padding-block: 6.825em;
+	border: 1px solid;
+	position: relative;
+	/* for making the container 100vw */
+	width: 100vw;
+	margin: 0 calc(-50vw + 50%);
+	/* for making the container 100vw */
+
+	.background {
+		display: none;
+		@media screen and (${devices["2xl"]}) {
+			display: block;
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			background: url(${BGImage}), lightgray 50% / cover no-repeat;
+			/* opacity: 0.5; */
+			filter: blur(15px);
+		}
+	}
+
+	.header {
+		display: flex;
+		flex-direction: column;
+		gap: 0.625em;
+	}
+
+	.cards_container {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	.chefCard--wrapper {
+		width: 47%;
+		@media screen and (${devices.xl}) {
+			width: 30%;
+		}
+	}
+`;
