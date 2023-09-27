f450fa0b1290fa6450912342b359b6cc3fa2a0fb
nazmus767921 <nazmus767921@gmail.com>
2023-09-27 13:34:32 +0600

    M: data provided

--- src/components/UI/Footer/components/FooterExtras/FooterSocialSection/FooterSocialSection.jsx
+++ src/components/UI/Footer/components/FooterExtras/FooterSocialSection/FooterSocialSection.jsx
@@ -1,10 +1,11 @@
+import { footer } from "../../../../../../constant/en-us/shared.constants";
 import SocialIcon from "../../../../SocialIcon";
 import { Wrapper } from "./style/FooterSocialSection.styles";
 
 const FooterSocialSection = () => {
 	return (
 		<Wrapper>
-			<SocialIcon />
+			<SocialIcon>{footer.socials[0].icon}</SocialIcon>
 		</Wrapper>
 	);
 };
