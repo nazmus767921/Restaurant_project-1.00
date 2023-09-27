55fd8b5bbf66d10d683436c7e5dc28b50ae00c3f
nazmus767921 <nazmus767921@gmail.com>
2023-09-27 21:09:25 +0600

    Reordered styles property

--- src/components/UI/MenuItem/styles/MenuItem.styles.js
+++ src/components/UI/MenuItem/styles/MenuItem.styles.js
@@ -2,30 +2,31 @@ import styled from "styled-components";
 import { colors } from "../../../../utils/colors";
 
 export const Wrapper = styled.div`
-	display: flex;
+	/* layout */
+	width: 100%;
 	padding: 0.625rem 0.65rem;
+	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	text-align: center;
 	gap: 0.625rem;
-	width: 100%;
 
+	/* typo */
+	text-align: center;
+
+	/* elements inside */
 	.item_image {
+		/* layout */
 		display: flex;
 		justify-content: center;
 		align-items: center;
-
 		padding-inline: 0.65em;
 		margin-block-end: 0.65em;
-
 		overflow: hidden;
 	}
-
 	.description {
 		opacity: 0.7;
 	}
-
 	.item_price {
 		color: ${colors.brand};
 	}
