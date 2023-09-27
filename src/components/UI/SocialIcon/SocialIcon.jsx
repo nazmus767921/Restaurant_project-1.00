b96eb6ac870725472a9bc3a5aa846e66dc705dcd
nazmus767921 <nazmus767921@gmail.com>
2023-09-27 13:50:26 +0600

    M: social icons made clickable links && styles

--- src/components/UI/SocialIcon/SocialIcon.jsx
+++ src/components/UI/SocialIcon/SocialIcon.jsx
@@ -1,7 +1,11 @@
 import { Wrapper } from "./style/SocialIcon.styles";
 
-const SocialIcon = ({ children, className = null }) => {
-	return <Wrapper className={className}>{children}</Wrapper>;
+const SocialIcon = ({ children, className = null, link }) => {
+	return (
+		<Wrapper className={className} href={link}>
+			{children}
+		</Wrapper>
+	);
 };
 
 export default SocialIcon;
--- src/components/UI/SocialIcon/style/SocialIcon.styles.js
+++ src/components/UI/SocialIcon/style/SocialIcon.styles.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 import { devices } from "../../../../utils/breakpoints";
+import { ease } from "../../../../utils/animation";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.a`
 	/* layout */
 	width: 1.75em;
 	aspect-ratio: 1;
 	flex-shrink: 0;
 	/* makeup */
 	opacity: 0.5;
+	/* animation */
+	transition: all 0.35s ${ease["out-expo"]};
+
+	/* events */
+	&:hover,
+	:focus {
+		opacity: 1;
+	}
 
 	/* media queries */
 	@media screen and (${devices["2xl"]}) {
