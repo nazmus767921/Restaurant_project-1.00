c9691960f1d05fd2719968aacd726da8a0551a55
nazmus767921 <nazmus767921@gmail.com>
2023-09-27 22:41:12 +0600

    Add error element

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import HomePage from "./pages/Home/HomePage";
+import Error from "./pages/Error/Error";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+		errorElement: <Error />,
 		children: [
 			{
 				path: "/",
