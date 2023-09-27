b86cd869a18fba81283ca23e4040bcd1c0969919
nazmus767921 <nazmus767921@gmail.com>
2023-09-27 23:16:38 +0600

    header max-width

--- src/pages/Error/styles/Error.styles.js
+++ src/pages/Error/styles/Error.styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { devices } from "../../../utils/breakpoints";
 
 export const Wrapper = styled.div`
 	display: flex;
@@ -16,10 +17,12 @@ export const Wrapper = styled.div`
 		display: flex;
 		flex-direction: column;
 		gap: 1em;
+		max-width: 1280px;
 	}
 	h2.error_title {
 		font-size: 2.5em;
 		line-height: 1.4em;
+		text-align: center;
 	}
 	h2.error_title > span {
 		font-size: 180%;
