a6fda2d0f4d8eda2c9e37fae8bc7e7570f10a4a5
nazmus767921 <nazmus767921@gmail.com>
2023-09-28 15:21:36 +0600

    Link added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import NavBar from "../../components/UI/NavBar/NavBar";
 import Tag from "../../components/UI/Tag";
 import TextBTN from "../../components/UI/TextBTN";
 import Title from "../../components/UI/Title";
 import { errorhero } from "../../constant/en-us/error_page.constants";
 import { Wrapper } from "./styles/Error.styles";
+import { paths } from "../../Root.paths";
 
 const Error = () => {
 	return (
@@ -14,7 +16,9 @@ const Error = () => {
 					<Tag>{errorhero.tagline}</Tag>
 					<Title className="error_title">{errorhero.title}</Title>
 				</div>
-				<TextBTN>{errorhero.btn}</TextBTN>
+				<Link to={paths.home}>
+					<TextBTN>{errorhero.btn}</TextBTN>
+				</Link>
 			</Wrapper>
 		</>
 	);
