92dc3d938149c08495043c2097f220be5fa2f436
nazmus767921 <nazmus767921@gmail.com>
2023-09-29 13:27:41 +0600

    gap adjusted for larger devices

--- src/components/UI/Footer/components/FooterExtras/FooterSocialSection/style/FooterSocialSection.styles.js
+++ src/components/UI/Footer/components/FooterExtras/FooterSocialSection/style/FooterSocialSection.styles.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
+import { devices } from "../../../../../../../utils/breakpoints";
 
 export const Wrapper = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	gap: 1.88em;
+	@media screen and (${devices.xl}) {
+		gap: 1.125em;
+	}
 `;
