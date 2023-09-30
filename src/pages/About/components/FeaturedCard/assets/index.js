5504ee30bbd6005cc5697ca3dca4dd971205995f
nazmus767921 <nazmus767921@gmail.com>
2023-09-30 23:02:37 +0600

    setup images export

---
+++ src/pages/About/components/FeaturedCard/assets/index.js
@@ -0,0 +1,5 @@
+import vegBowl from "./bowl-vegetables_squared 1.jpg";
+import ChickenBowl from "./chicken-sticks-bowl-tomato-soup 1.jpg";
+import FryingOnStove from "./frying-with-stove-spice 1.jpg";
+
+export { vegBowl, ChickenBowl, FryingOnStove };
