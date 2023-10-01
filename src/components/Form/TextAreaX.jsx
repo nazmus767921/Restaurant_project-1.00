9fdfc71bbdadcef1d60609e8f7349181772c8dc3
nazmus767921 <nazmus767921@gmail.com>
2023-10-01 19:13:55 +0600

    updated

--- src/components/Form/TextAreaX.jsx
+++ src/components/Form/TextAreaX.jsx
@@ -4,12 +4,13 @@ import styled from "styled-components";
 const TextAreaX = ({
 	name = null,
 	value = null,
-	placeholder = "type here",
+	placeholder = "Please, tell us here",
 	label,
 	onChange,
+	maxLength = null,
 }) => {
 	const handleInputChange = (e) => {
-		onChange();
+		// onChange();
 		e.target.style.height = "auto";
 		e.target.style.height = `${e.target.scrollHeight}px`;
 	};
@@ -20,6 +21,8 @@ const TextAreaX = ({
 				{label || name}
 			</label>
 			<textarea
+				maxLength={maxLength}
+				rows="1"
 				id={name}
 				name={name}
 				placeholder={placeholder}
@@ -41,13 +44,18 @@ const Wrapper = styled.div`
 		color: ${colors.brand};
 		text-transform: capitalize;
 	}
-	input {
+	textarea {
 		/* layout */
+		max-width: 100%;
+		min-width: 100%;
+		min-height: 2.5em;
+		max-height: 16em;
 		border: 0;
 		border-bottom: 2px solid ${colors.brand};
 		background-color: transparent;
 		/* typo */
 		font-size: 1em;
+		font-family: "Libre Franklin", sans-serif;
 		line-height: 2em;
 	}
 `;
