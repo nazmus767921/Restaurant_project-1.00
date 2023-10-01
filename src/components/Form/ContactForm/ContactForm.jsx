1b1aaedef9624b67249b468aeb8b8d931689cae4
nazmus767921 <nazmus767921@gmail.com>
2023-10-01 20:07:50 +0600

    complete

---
+++ src/components/Form/ContactForm/ContactForm.jsx
@@ -0,0 +1,30 @@
+import { Form } from "react-router-dom";
+import { FormWrapper } from "./styles/ContactForm.styles";
+import TextInput from "../TextInput";
+import TextAreaX from "../TextAreaX";
+import BTNSolid from "../../UI/BTNSolid";
+
+const ContactForm = () => {
+	return (
+		<FormWrapper>
+			<Form className="form-control">
+				<TextInput
+					type="text"
+					name="name"
+					placeholder="Your name, Sir/ Madam?"
+					required
+				/>
+				<TextInput
+					type="text"
+					name="email"
+					placeholder="Your Phone Number, Sir/ Madam?"
+					required
+				/>
+				<TextAreaX name="message" maxLength="500" />
+				<BTNSolid className="submit--btn">Submit</BTNSolid>
+			</Form>
+		</FormWrapper>
+	);
+};
+
+export default ContactForm;
---
+++ src/components/Form/ContactForm/styles/ContactForm.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FormWrapper = styled.div`
+	/* elements inside */
+	.form-control {
+		display: flex;
+		flex-direction: column;
+		gap: 2.5em;
+	}
+	.submit--btn {
+		margin: 0 auto;
+	}
+`;
