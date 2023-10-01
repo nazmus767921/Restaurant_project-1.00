a952fd8231c9271da63b8807b274b8cd842485b4
nazmus767921 <nazmus767921@gmail.com>
2023-10-01 18:44:53 +0600

    updated

--- src/pages/Contact/ContactPage.jsx
+++ src/pages/Contact/ContactPage.jsx
@@ -1,3 +1,4 @@
+import ContactForm from "../../components/Form/ContactForm/ContactForm";
 import { hero } from "../../constant/en-us/contact_page.constants";
 import { Main } from "./styles/ContactPage.styles";
 import spanTool from "span-tool";
@@ -9,6 +10,7 @@ const ContactPage = () => {
 				<h2>{spanTool(hero.title, "Please Let Us Know", "title--colored")}</h2>
 				<p className="description">{hero.description}</p>
 			</div>
+			<ContactForm />
 		</Main>
 	);
 };
