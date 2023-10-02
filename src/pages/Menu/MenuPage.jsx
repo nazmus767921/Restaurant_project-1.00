76ed9bc76acc5e86fc688b3e2ecdb84ff25784a6
nazmus767921 <nazmus767921@gmail.com>
2023-10-02 17:26:43 +0600

    updated

--- src/pages/Menu/MenuPage.jsx
+++ src/pages/Menu/MenuPage.jsx
@@ -1,7 +1,19 @@
+import FoodFilterMenu from "../../components/UI/Food Filter/FoodFilterMenu";
+import Hr from "../../components/UI/Hr";
+import Title from "../../components/UI/Title";
+import { title } from "../../constant/en-us/foodmenu_page.constants";
 import { Main } from "./styles/MenuPage.styles";
 
 const MenuPage = () => {
-	return <Main>MenuPage</Main>;
+	return (
+		<Main className="page-container">
+			<div className="header">
+				<Title>{title}</Title>
+				<Hr />
+			</div>
+			<FoodFilterMenu />
+		</Main>
+	);
 };
 
 export default MenuPage;
--- src/pages/Menu/styles/MenuPage.styles.js
+++ src/pages/Menu/styles/MenuPage.styles.js
@@ -1,3 +1,16 @@
 import styled from "styled-components";
+import { devices } from "../../../utils/breakpoints";
 
-export const Main = styled.main``;
+export const Main = styled.main`
+	display: flex;
+	flex-direction: column;
+	gap: 1.25em;
+	/* elements inside */
+	.header {
+		width: fit-content;
+		margin: 0 auto;
+		@media screen and (${devices.md}) {
+			margin-top: 4em;
+		}
+	}
+`;
