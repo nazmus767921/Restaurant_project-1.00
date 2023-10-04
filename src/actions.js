839ec81ab1d5f194a0ea286b414d62a36c0e06be
nazmus767921 <nazmus767921@gmail.com>
2023-10-04 15:46:42 +0600

    updated

--- src/actions.js
+++ src/actions.js
@@ -2,3 +2,4 @@ export const UPDATE_FILTERS = "UPDATE_FILTERS";
 export const FETCH_MENUS = "FETCH_MENUS";
 export const LOAD_PRODUCTS = "LOAD_PRODUCTS";
 export const FILTER_MENUS = "FILTER_MENUS";
+export const UPDATE_SORT = "UPDATE_SORT";
