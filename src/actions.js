0315be10325e1d1a08ccdbba8ecf86e5e0be8faa
nazmus767921 <nazmus767921@gmail.com>
2023-10-04 19:07:02 +0600

    updated documentations

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,8 @@
-export const FETCH_MENUS = "FETCH_MENUS";
-export const LOAD_FILTERED_MENU = "LOAD_FILTERED_MENU";
-export const UPDATE_FILTERS = "UPDATE_FILTERS";
-export const FILTER_MENUS = "FILTER_MENUS";
-export const UPDATE_SORT = "UPDATE_SORT";
-export const SORT_MENU = "SORT_MENU";
-export const UPDATE_GRID_VIEW = "UPDATE_GRID_VIEW";
-export const UPDATE_LIST_VIEW = "UPDATE_LIST_VIEW";
-export const GET_CATEGORIES = "GET_CATEGORIES";
+export const FETCH_MENUS = "FETCH_MENUS"; // fetch all menus on initial load.
+export const UPDATE_FILTERS = "UPDATE_FILTERS"; // sets filtering condition in state.
+export const FILTER_MENUS = "FILTER_MENUS"; // filters the menu item based on the state.filters conditions.
+export const UPDATE_SORT = "UPDATE_SORT"; // sets the sorting condition in the state
+export const SORT_MENU = "SORT_MENU"; // sorts the menu item based on the state.sort condition.
+export const UPDATE_GRID_VIEW = "UPDATE_GRID_VIEW"; // sets the grid view for menu items in menus page.
+export const UPDATE_LIST_VIEW = "UPDATE_LIST_VIEW"; // sets the list view for menu items in menus page.
+export const GET_CATEGORIES = "GET_CATEGORIES"; // get the unique categories from state.menus on initial load.
