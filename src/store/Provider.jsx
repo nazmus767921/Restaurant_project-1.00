d2ab193a6b24f738300305e9b758a50f0ae399cd
nazmus767921 <nazmus767921@gmail.com>
2023-10-05 13:42:46 +0600

    added pagination cprovider

--- src/store/Provider.jsx
+++ src/store/Provider.jsx
@@ -1,7 +1,12 @@
+import { PaginationContextProvider } from "./contexts/pagination_context";
 import { Products_contextProvider } from "./contexts/products_context";
 
 const GlobalContextProvider = ({ children }) => {
-	return <Products_contextProvider>{children}</Products_contextProvider>;
+	return (
+		<Products_contextProvider>
+			<PaginationContextProvider>{children}</PaginationContextProvider>
+		</Products_contextProvider>
+	);
 };
 
 export default GlobalContextProvider;
