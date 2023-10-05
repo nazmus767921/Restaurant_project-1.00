ebde3712dc10df3ad376aff7d5f4530d9dd0f4f8
nazmus767921 <nazmus767921@gmail.com>
2023-10-05 20:01:20 +0600

    init

---
+++ src/components/Form/Label.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Label = styled.label`
+	font-size: 1.125rem;
+	font-weight: 700;
+	text-transform: capitalize;
+`;
+
+export default Label;
---
+++ src/components/Form/SelectX.jsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { colors } from "../../utils/colors";
+import { IoMdArrowDropdown } from "react-icons/io";
+import Label from "./Label";
+
+const SelectX = ({ name, id, label, value, onChange, options = [] }) => {
+	return (
+		<Wrapper>
+			<Label htmlFor={id || name}>{label}</Label>
+			<ElementContainer>
+				<div className="icon--wrapper">
+					<IoMdArrowDropdown style={{ color: colors.brand }} />
+				</div>
+				<SelectWrapper
+					name={name}
+					id={id || name}
+					value={value}
+					onChange={onChange}
+				>
+					{options.map((option) => {
+						return (
+							<option key={option?.value} value={option?.value}>
+								{option?.label}
+							</option>
+						);
+					})}
+				</SelectWrapper>
+			</ElementContainer>
+		</Wrapper>
+	);
+};
+
+const SelectWrapper = styled.select`
+	/* layout */
+	border: 0;
+	border-bottom: 2px solid ${colors.brand};
+	background-color: ${colors["bg-primary-dark"]};
+	/* typo */
+	font-size: 1em;
+	line-height: 2em;
+
+	color: inherit;
+	width: 100%;
+	/* makeup */
+	appearance: none;
+`;
+
+const ElementContainer = styled.div`
+	position: relative;
+	.icon--wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		padding: 0.25em;
+		/* elements inside */
+	}
+`;
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1em;
+`;
+
+export default SelectX;
