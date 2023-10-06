00896b2171aba8fb0ca862e641695f0029bd8509
nazmus767921 <nazmus767921@gmail.com>
2023-10-06 13:46:06 +0600

    uploaded images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,7 @@
 import WaveDeco from "./Wavy_decorator.png";
 import Shahi_kebab from "./sahi_kebab.png";
+import Salad from "./salad.png";
+import TurkeyMeat from "./turkey_meat.png";
+import KimchiSoup from "./kimchi-soup.png";
 
-export { WaveDeco, Shahi_kebab };
+export { WaveDeco, Shahi_kebab, Salad, TurkeyMeat, KimchiSoup };
